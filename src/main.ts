b4a020c478b9dcf90fac4264e64bbcf7445a103c
MHP24 <mig.henriquezp@gmail.com>
2024-05-16 12:42:59 -0400

    chore: dir structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from './config';
+import { envs } from './common/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
