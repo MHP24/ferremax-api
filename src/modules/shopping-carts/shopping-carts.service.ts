83e20a0c0afccbdf1051030ad0d50b77e8a8e918
MHP24 <mig.henriquezp@gmail.com>
2024-05-19 01:24:30 -0400

    feat(/shopping-carts): add base methods required in orders module

---
+++ src/modules/shopping-carts/shopping-carts.service.ts
@@ -0,0 +1,63 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { type ShoppingCart } from './types';
+import { type ShoppingCart as PrismaShoppingCart } from '@prisma/client';
+
+@Injectable()
+export class ShoppingCartsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  // * Get shopping cart by userId
+  async getUserShoppingCart(userId: string): Promise<ShoppingCart> {
+    const userShoppingCart = await this.prismaService.shoppingCart.findFirst({
+      where: {
+        userId,
+        isActive: true,
+      },
+      include: {
+        ShoppingCartItem: {
+          include: {
+            Product: {
+              select: {
+                productId: true,
+                name: true,
+                slug: true,
+                price: true,
+                stock: true,
+                images: true,
+                isActive: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    // * Cart existence validation
+    if (!userShoppingCart) {
+      throw new BadRequestException('Shopping cart not found');
+    }
+
+    // * Cart formatting
+    const { ShoppingCartItem: items, ...rest } = userShoppingCart;
+    return {
+      ...rest,
+      items: items.map(({ Product, quantity }) => ({
+        product: Product,
+        quantity,
+      })),
+    };
+  }
+
+  // * Disable user cart by cartId
+  async disableShoppingCartById(cartId: string): Promise<PrismaShoppingCart> {
+    return await this.prismaService.shoppingCart.update({
+      where: {
+        cartId,
+      },
+      data: {
+        isActive: false,
+      },
+    });
+  }
+}
