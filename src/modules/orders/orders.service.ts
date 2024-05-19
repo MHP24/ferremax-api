a9548ca65d0e892e3f59f451b6aacd5708b78dd3
MHP24 <mig.henriquezp@gmail.com>
2024-05-18 20:14:12 -0400

    perf(/orders): query for shopping cart

--- src/modules/orders/orders.service.ts
+++ src/modules/orders/orders.service.ts
@@ -13,91 +13,106 @@ export class OrdersService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async createOrder({ id: userId }: User): Promise<OrderProcessed> {
-    try {
-      // * Validate if user has a shopping cart active
-      // TODO: Use ShoppingCartService
-      const shoppingCart = await this.prismaService.shoppingCart.findFirst({
-        where: {
-          userId,
-          isActive: true,
-        },
-      });
-
-      if (!shoppingCart) {
-        throw new BadRequestException('Shopping cart not found');
-      }
-
-      // * Get items in cart
-      // TODO: Use ShoppingCartService
-      const itemsInCart = await this.prismaService.shoppingCartItem.findMany({
-        where: { cartId: shoppingCart.cartId },
-        select: {
-          productId: true,
-          quantity: true,
-        },
-      });
-
-      // * Get items with the current price updated
-      // TODO: Use ProductsService
-      const products = await this.prismaService.product.findMany({
-        where: {
-          isActive: true,
-          productId: {
-            in: itemsInCart.map(({ productId }) => productId),
+    // * Validate if user has a shopping cart active
+    // TODO: Use ShoppingCartService
+    const userShoppingCart = await this.prismaService.shoppingCart.findFirst({
+      where: {
+        userId,
+        isActive: true,
+      },
+      include: {
+        ShoppingCartItem: {
+          include: {
+            Product: {
+              select: {
+                productId: true,
+                name: true,
+                slug: true,
+                price: true,
+                stock: true,
+                images: true,
+                isActive: true,
+              },
+            },
           },
         },
-      });
+      },
+    });
 
-      // * Check if all products are active and available to buy (without considering stock)
-      if (products.length !== itemsInCart.length) {
-        throw new BadRequestException(
-          'Some products are not available, check your shopping cart',
+    if (!userShoppingCart) {
+      throw new BadRequestException('Shopping cart not found');
+    }
+
+    const { ShoppingCartItem: items, ...rest } = userShoppingCart;
+    const shoppingCart = {
+      ...rest,
+      items: items.map(({ Product, quantity }) => ({
+        product: Product,
+        quantity,
+      })),
+    };
+
+    // * Get items with the current price updated
+    // TODO: Use ProductsService
+    const products = await this.prismaService.product.findMany({
+      where: {
+        isActive: true,
+        productId: {
+          in: shoppingCart.items.map(({ product }) => product.productId),
+        },
+      },
+    });
+
+    // * Check if all products are active and available to buy (without considering stock)
+    if (products.length !== shoppingCart.items.length) {
+      throw new BadRequestException(
+        'Some products are not available, check your shopping cart',
+      );
+    }
+
+    // * Stock validation before order creation
+    const areProductsInStock = shoppingCart.items.every(
+      ({ product: { productId: cartProductId }, quantity }) => {
+        return (
+          products.find(({ productId }) => productId === cartProductId).stock >=
+          quantity
         );
-      }
+      },
+    );
 
-      // * Stock validation before order creation
-      const areProductsInStock = itemsInCart.every(
-        ({ productId: cartProductId, quantity }) => {
-          return (
-            products.find(({ productId }) => productId === cartProductId)
-              .stock >= quantity
-          );
-        },
-      );
+    if (!areProductsInStock) {
+      throw new BadRequestException('Some products are not in stock');
+    }
 
-      if (!areProductsInStock) {
-        throw new BadRequestException('Some products are not in stock');
-      }
+    const orderId = this.idGenerator.id('FER-ORDER');
 
-      const orderId = this.idGenerator.id('FER-ORDER');
+    // * Order items with subtotal calc
+    const orderItems = shoppingCart.items.reduce(
+      (acc: OrderItem[], { product: { productId }, quantity }) => {
+        const { name, price, images } = products.find(
+          (product) => product.productId === productId,
+        );
+        return [
+          ...acc,
+          {
+            name,
+            thumbnail: images[0],
+            productId,
+            price,
+            subtotal: quantity * price,
+            quantity,
+          },
+        ];
+      },
+      [],
+    );
 
-      // * Order items with subtotal calc
-      const orderItems = itemsInCart.reduce(
-        (acc: OrderItem[], { productId, quantity }) => {
-          const { name, price, images } = products.find(
-            (product) => product.productId === productId,
-          );
-          return [
-            ...acc,
-            {
-              name,
-              thumbnail: images[0],
-              productId,
-              price,
-              subtotal: quantity * price,
-              quantity,
-            },
-          ];
-        },
-        [],
-      );
-
-      // * Order total
-      const total = orderItems.reduce(
-        (acc, { subtotal }) => (acc += subtotal),
-        0,
-      );
-
+    // * Order total
+    const total = orderItems.reduce(
+      (acc, { subtotal }) => (acc += subtotal),
+      0,
+    );
+    try {
       const [order] = await this.prismaService.$transaction([
         // * Order creation
         this.prismaService.order.create({
@@ -123,7 +138,6 @@ export class OrdersService {
       // TODO: Use StockService Update stock
 
       // TODO: Use ShoppingCartService Clear & disable cart
-
       return {
         order,
         items: orderItems,
@@ -131,7 +145,7 @@ export class OrdersService {
     } catch (error) {
       this.logger.error(error);
       throw new BadRequestException(
-        'An unexpected error occurred trying to generate order, try again',
+        'An unexpected error occurred trying to generate the order, try again',
       );
     }
   }
