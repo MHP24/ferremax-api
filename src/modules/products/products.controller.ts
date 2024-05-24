e7866492205b6856ef7232b46a321887c212ae1a
MHP24 <mig.henriquezp@gmail.com>
2024-05-24 02:15:41 -0400

    feat: add find all products (pagination supported)

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { PaginationDto } from '../../common/dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  findManyProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
+  }
 }
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,9 +1,43 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { PaginationDto } from '../../common/dto';
 
 @Injectable()
 export class ProductsService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  // * Find many products (supports pagination)
+  async findAll(paginationDto: PaginationDto) {
+    // * Pages and last page calc
+    const totalPages = await this.prismaService.product.count({});
+    const { page, limit } = paginationDto;
+    const lastPage = Math.ceil(totalPages / limit);
+
+    // * Products with all info
+    const products = await this.prismaService.product.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      select: {
+        name: true,
+        slug: true,
+        price: true,
+        images: true,
+        isActive: true,
+        stock: true,
+      },
+    });
+
+    // * pagination metadata and products info
+    return {
+      data: products,
+      meta: {
+        page,
+        results: products.length,
+        total: totalPages,
+        lastPage,
+      },
+    };
+  }
 
   // * Find many products by id as array of ids arg
   async findManyProductsById(ids: string[]) {
