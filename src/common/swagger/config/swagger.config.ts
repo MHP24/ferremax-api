b7b6d46ba670383a1f3ae4b44ef51a89723033fa
MHP24 <mig.henriquezp@gmail.com>
2024-05-25 15:04:29 -0400

    docs: update swagger description conf

--- src/common/swagger/config/swagger.config.ts
+++ src/common/swagger/config/swagger.config.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 export const swaggerConfig = (prefix: string, app: INestApplication) => {
   const config = new DocumentBuilder()
     .setTitle('Ferremax API')
-    .setDescription('Ferremax API specification')
+    .setDescription(
+      'Ferremax API, manage your orders, authentication, products, and shopping cart efficiently and quickly at your trusted hardware store.',
+    )
     .addBearerAuth()
     .setVersion('1.0')
     .build();
